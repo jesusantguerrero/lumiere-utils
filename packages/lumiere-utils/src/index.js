b2c76dd8984ee734fd187ae6eecc40d97b7b45b0
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-10-09 08:38:33 -0400

    exclude files with dependencies

--- packages/lumiere-utils/src/index.js
+++ packages/lumiere-utils/src/index.js
@@ -1,5 +1,3 @@
-export { useFirebase } from './providers/firebase/useFirebase'
-export { useSupabase } from './providers/supabase/useSupabase'
 export { useLumiere } from "./useLumiere"
 export { defaultUserButtonOptions } from "./utils"
 export { useAuth, AuthState } from './useAuth'

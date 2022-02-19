72ffb8a32123dc10105cc20233f3276ac094cf82
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-02-19 08:20:35 -0400

    🔖 0.3.11

--- packages/lumiere-utils/src/index.js
+++ packages/lumiere-utils/src/index.js
@@ -2,3 +2,5 @@ export { useLumiere } from "./useLumiere"
 export { defaultUserButtonOptions } from "./utils"
 export { useAuth, AuthState } from './useAuth'
 export { default as LumiereProvider } from './components/LumiereProvider.vue'
+export { useSupabase } from "./providers/supabase/useSupabase"
+export { useFirebase } from "./providers/firebase/useFirebase"
